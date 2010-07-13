9116d6a7f5064271bc43348ed28d2fe8898249d1
Hendel <dwarf.hendel@gmail.com>
2010-07-14 00:46:27 +0200

    Updated the settings for the DB path and the compass processor.

--- assets/sass/config.rb
+++ assets/sass/config.rb
@@ -1,9 +1,9 @@
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../htdocs/styles"
+css_dir = "../../media/styles"
 sass_dir = "."
-images_dir = "../../htdocs/images"
-javascripts_dir = "../../htdocs/scripts"
+images_dir = "../../media/images"
+javascripts_dir = "../../media/scripts"
 output_style = :expanded
 relative_assets = true
